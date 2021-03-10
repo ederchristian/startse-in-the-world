b2758128e72d29e0e024e821bc9255109c504eb5
Eder Christian <ederchristian92@gmail.com>
2021-03-10 00:31:23 -0300

    style: add new general variables

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,6 +1,18 @@
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyles = createGlobalStyle`
+  :root {
+    --highlight: #fd6269;
+    --background: #030518;
+    --white: #eeeeee;
+
+    --container: 100rem;
+
+    --small: 1.5rem;
+    --medium: 3rem;
+    --large: 5rem;
+  }
+
   * {
     margin: 0;
     padding: 0;
@@ -13,6 +25,8 @@ const GlobalStyles = createGlobalStyle`
 
   html, body, #__next {
     height: 100%;
+    background: var(--background);
+    color: var(--white);
   }
 
   body {
@@ -23,6 +37,32 @@ const GlobalStyles = createGlobalStyle`
   h1, h2, h3, a, button {
     font-family: 'Inter', sans-serif;
     font-weight: 700;
+  }
+
+  h1, h2, h3 {
+    line-height: var(--large);
+  }
+
+  h1 {
+    font-size: 4rem;
+  }
+
+  h2 {
+    font-size: 3.4rem;
+  }
+
+  h3 {
+    font-size: 2.6rem;
+  }
+
+  p,
+  a {
+    font-size: 1.6rem;
+    line-height: var(--medium);
+  }
+
+  a {
+    color: var(--highlight);
   }
 `
 
