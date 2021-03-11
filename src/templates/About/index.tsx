752e75c5644e6466522c6f34731602b44de1f0f4
Eder Christian <ederchristian92@gmail.com>
2021-03-11 01:08:35 -0300

    feat: add the About template

---
+++ src/templates/About/index.tsx
@@ -0,0 +1,25 @@
+import { CloseOutline } from '@styled-icons/evaicons-outline/CloseOutline'
+
+import LinkWrapper from 'components/LinkWrapper'
+
+import * as Styled from './styles'
+
+const AboutTemplate = () => (
+  <Styled.Content>
+    <LinkWrapper href="/">
+      <CloseOutline size={32} aria-label="Close" />
+    </LinkWrapper>
+
+    <Styled.Heading>StartSe in the World</Styled.Heading>
+    <Styled.Body>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum libero
+        cumque alias odio aspernatur expedita adipisci quibusdam doloribus odit
+        velit et sequi nulla voluptatibus quis aliquam, obcaecati nemo, vero
+        illum.
+      </p>
+    </Styled.Body>
+  </Styled.Content>
+)
+
+export default AboutTemplate
---
+++ src/templates/About/styles.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const Content = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  height: 100vh;
+  max-width: var(--container);
+  margin: auto;
+`
+
+export const Heading = styled.h1`
+  margin-bottom: var(--medium);
+`
+
+export const Body = styled.div``
