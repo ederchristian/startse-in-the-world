257dbee81cd761931952c12bb45936cbb4746145
Eder Christian <ederchristian92@gmail.com>
2021-03-12 00:10:58 -0300

    feat: add items from GraphQL

--- src/templates/Pages/index.tsx
+++ src/templates/Pages/index.tsx
@@ -4,20 +4,20 @@ import LinkWrapper from 'components/LinkWrapper'
 
 import * as Styled from './styles'
 
-const PageTemplate = () => (
+export type PageTemplateProps = {
+  heading: string
+  body: string
+}
+
+const PageTemplate = ({ heading, body }: PageTemplateProps) => (
   <Styled.Content>
     <LinkWrapper href="/">
       <CloseOutline size={32} aria-label="Close" />
     </LinkWrapper>
 
-    <Styled.Heading>StartSe in the World</Styled.Heading>
+    <Styled.Heading>{heading}</Styled.Heading>
     <Styled.Body>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum libero
-        cumque alias odio aspernatur expedita adipisci quibusdam doloribus odit
-        velit et sequi nulla voluptatibus quis aliquam, obcaecati nemo, vero
-        illum.
-      </p>
+      <div dangerouslySetInnerHTML={{ __html: body }} />
     </Styled.Body>
   </Styled.Content>
 )
