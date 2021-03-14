e04b34b9846948139a340dec5a6ee94a9f62f975
Eder Christian <ederchristian92@gmail.com>
2021-03-14 19:51:46 -0300

    feat: add the config file for SEO

---
+++ next-seo.config.ts
@@ -0,0 +1,13 @@
+export default {
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://ederchristian.com/startse-in-the-world',
+    site_name: 'StartSe in the World'
+  },
+  twitter: {
+    handle: '@ederchristian92',
+    site: '@site',
+    cardType: 'summary_large_image'
+  }
+}
